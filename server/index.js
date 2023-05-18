3302f665e40ba0fd215466dbfd1aa77f81c28168
Orxan228 <orxan.qasimov.2003@gmail.com>
2023-05-18 11:10:12 +0400

    add

--- server/index.js
+++ server/index.js
@@ -3,135 +3,172 @@ const app = express();
 const crypto = require("crypto");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+dotenv.config();
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-
-// const dotenv = require('dotenv');
-// dotenv.config();
-
-const PORT = 8080;
-
-//data
-const STUDENTS = [
-  {
-    id: 1,
-    name: "Ayshen",
-    surname:
-      "EZizova",
-    age: 21,
-    birthday:"31dec"
-  },
-  {
-    id: 2,
-    name: "Kanye",
-    surname:
-      "Eova",
-    age: 45,
-    birthday:"2dec"
-  },
-  {
-    id: 3,
-    name: "Eziz",
-    surname:
-      "Yovaa",
-    age: 35,
-    birthday:"3jan"
-  },
-];
-
-app.get("/api", (req, res) => {
-  res.send("Welcome to Our API!");
+//Artist Schema
+const ArtistSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  imageURL: String
 });
+//Song Schema
+const SongSchema = new mongoose.Schema({
+  title: String,
+  duration: Number,
+  cover: String,
+  releaseYear: Number,
+  artistID: {type: mongoose.Schema.Types.ObjectId, ref: 'Artists'}
+})
+
+//Artist Model
+const ArtistModel = mongoose.model('Artists',ArtistSchema);
+//Song Model
+const SongModel = mongoose.model('Songs',SongSchema);
+//MONGO DATABASE CONNECTION
+DB_CONNECTION = process.env.DB_CONNECTION
+DB_PASSWORD = process.env.DB_PASSWORD
+mongoose.connect(DB_CONNECTION.replace("<password>",DB_PASSWORD))
+.then(()=> console.log("Mongo DB Connected!"))
+
+
 
 //CRUD - CREATE READ UPDATE DELETE
 //STATUS CODES - 200,201,202, 404, 204
+app.get("/api", (req, res) => {
+  res.send("Welcome to Our API!");
+});
+//get All Artists
+app.get("/api/artists", async(req, res) => {
 
-//get All Students
-app.get("/api/students", (req, res) => {
-  const { name } = req.query;
-  if (name === undefined) {
-    res.status(200).send({
-      data: STUDENTS,
-      message: "data get success!",
-    });
+  const { page, limit } = req.query;
+  const artists = await ArtistModel.find();
+  if (page & limit) {
+    let start = (page - 1) * limit;
+    let end = page * limit;
+    res.send(artists.slice(start, end));
   } else {
     res.status(200).send({
-      data: STUDENTS.filter(
-        (x) => x.name.toLowerCase().trim().includes(name.toLowerCase().trim())
-      ),
+      data: artists,
       message: "data get success!",
     });
   }
+  
+  
+    // const { name } = req.query;
+    // const artists = await ArtistModel.find();
+    // if (name === undefined) {
+    //   res.status(200).send({
+    //     data: artists,
+    //     message: "data get success!",
+    //   });
+    // } else {
+    //   res.status(200).send({
+    //     data: artists.filter((x)=>x.name.toLowerCase().trim().includes(name.toLowerCase().trim())),
+    //     message: "data get success!",
+    //   });
+    // }
 });
-//get student by ID
-app.get("/api/students/:id", (req, res) => {
+//get Artists by ID
+app.get("/api/artists/:id", async(req, res) => {
   const id = req.params.id;
-  const student = STUDENTS.find((x) => x.id === parseInt(id));
-  if (!student) {
-    console.log("test");
-    res.status(204).send("student not found!");
-    // return;
+  const artist = await ArtistModel.findById(id);
+  console.log('artist found: ',artist);
+  if (!artist) {
+    res.status(204).send("artist not found!");
   } else {
     res.status(200).send({
-      data: student,
+      data: artist,
       message: "data get success!",
     });
-    // return;
   }
 });
 //delete artist by ID
-app.delete("/api/students/:id", (req, res) => {
+app.delete("/api/artists/:id", async(req, res) => {
   const id = req.params.id;
-  const student = STUDENTS.find((x) => x.id == id);
-  if (student === undefined) {
-    res.status(404).send("student not found");
+  const artist = await ArtistModel.findByIdAndDelete(id);
+  if (artist === undefined) {
+    res.status(404).send("artist not found");
   } else {
-    const idx = STUDENTS.indexOf(student);
-    STUDENTS.splice(idx, 1);
     res.status(203).send({
-      data: student,
-      message: "student deleted successfully",
+      data: artist,
+      message: "artist deleted successfully",
     });
   }
 });
 //post
-app.post("/api/students",(req, res) => {
-  const { name, age, surname, birthday } = req.body;
-  const newStudents= {
-    id: crypto.randomUUID(),
+app.post("/api/artists",async(req, res) => {
+  const { name, age, imageURL } = req.body;
+  const newArtist = new ArtistModel({
     name: name,
-    surname: surname,
+    imageURL: imageURL,
     age: age,
-    birthday:birthday,
-  };
-  STUDENTS.push(newStudents);
+  })
+  await newArtist.save();
   res.status(201).send("created");
 });
 //put
-app.put("/api/students/:id", (req, res) => {
+app.put("/api/artists/:id", (req, res) => {
   const id = req.params.id;
   const { name, age, imageURL } = req.body;
-  const existedStudent = STUDENTS.find((x) => x.id == id);
-  if (existedStudent == undefined) {
-    res.status(404).send("student not found!");
+  const existedArtist = ARTISTS.find((x) => x.id == id);
+  if (existedArtist == undefined) {
+    res.status(404).send("artist not found!");
   } else {
     if (name) {
-      existedStudent.name = name;
+      existedArtist.name = name;
     }
     if (age) {
-      existedStudent.age = age;
+      existedArtist.age = age;
     }
     if (imageURL) {
-      existedStudent.imageURL = imageURL;
+      existedArtist.imageURL = imageURL;
     }
-    res.status(200).send(`artist: ${existedStudent.name}`);
+    res.status(200).send(`artist: ${existedArtist.name}`);
   }
 });
 
-// PORT  = process.env.PORT;
+//SONGS------------------------------------
+app.get("/api/songs/:id",async(req,res)=>{
+  const id = req.params.id;
+  const songs = await SongModel.find();
+  if (songs==undefined) {
+    res.status(404).send("songs not found!");
+  }
+  else{
+    res.status(200).send(songs.filter((song)=>song.artistID==id));
+  }
+})
+//get all songs
+app.get("/api/songs/",async(req,res)=>{
+  const songs = await SongModel.find();
+  if (songs==undefined) {
+    res.status(404).send("songs not found!");
+  }
+  else{
+    res.status(200).send(songs);
+  }
+})
+app.post("/api/songs",async(req,res)=>{
+  const{title,duration,cover,releaseYear,artistID} = req.body;
+
+  const song = new SongModel({
+    title: title,
+    duration: duration,
+    cover: cover,
+    releaseYear: releaseYear,
+    artistID: artistID
+  })
+  await song.save();
+  res.status(201).send("song created successfully");
+})
+
+PORT  = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`NODE APP listening on port ${PORT}`);
 });
